d99f047e4e5f7075d1b601a70db8d6f6c6f18746
Kazedesu1 <111736493+Kazedesu1@users.noreply.github.com>
2023-09-30 10:39:28 +0700

    Update Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,10 +1,7 @@
 #include<iostream>
 #include<math.h>
-
 using namespace std;
-int main ()
-{
-    class PHANSO
+class PHANSO
     {
         private:
         int tu,mau;
@@ -12,7 +9,7 @@ int main ()
         void nhap()
         {
             cout <<endl<<"nhap vao tu so: "; cin >> this->tu;
-            cout <<endl<<"nhap vao mau so: "; cin >> this->mau;
+            cout <<"nhap vao mau so: "; cin >> this->mau;
         }
         void in()
         {
@@ -48,6 +45,7 @@ int main ()
             tg.toigian();
             return (tg);
         }
+        
         PHANSO nhan(PHANSO &p2)
         {
             PHANSO tg;
@@ -64,14 +62,50 @@ int main ()
             tg.toigian();
             return (tg);
         }
+        PHANSO operator-()
+        {
+            PHANSO tg;
+            tg.tu = - this->tu;
+            tg.mau = this->mau;
+            return (tg);
+        }
+        bool sosanh(PHANSO p1,PHANSO p2){
+        return(float(p1.tu)/p1.mau<float(p2.tu)/p2.mau);
+        
+    }
+    friend bool operator<(PHANSO p1,PHANSO p2);
+        friend bool operator<=(PHANSO p1,PHANSO p2);
+        friend bool operator==(PHANSO p1,PHANSO p2);
+        friend bool operator>=(PHANSO p1,PHANSO p2);
+        friend bool operator>(PHANSO p1,PHANSO p2);
+        friend bool sosanh(PHANSO p1,PHANSO p2);
+    };
+    bool operator<(PHANSO p1,PHANSO p2){
+        return(float(p1.tu)/p1.mau<float(p2.tu)/p2.mau);
+    }
+        bool operator<=(PHANSO p1,PHANSO p2){
+        return(float(p1.tu)/p1.mau<=float(p2.tu)/p2.mau);
+    }
+        bool operator==(PHANSO p1,PHANSO p2){
+        return(float(p1.tu)/p1.mau==float(p2.tu)/p2.mau);
+    }
+        bool operator>=(PHANSO p1,PHANSO p2){
+        return(float(p1.tu)/p1.mau>=float(p2.tu)/p2.mau);
+    }
+        bool operator>(PHANSO p1,PHANSO p2){
+        return(float(p1.tu)/p1.mau>float(p2.tu)/p2.mau);
+    }
         bool sosanh(PHANSO p1,PHANSO p2){
         return(float(p1.tu)/p1.mau<float(p2.tu)/p2.mau);
     }
-    };
+using namespace std;
+int main ()
+{
+    
     int n;
-    cout <<endl<<"nhap so phan so: "; cin >> n;
+    cout <<"nhap so phan so: "; cin >> n;
     PHANSO *p = new PHANSO[n];
-    cout <<endl<<"nhap day cac phan so: ";
+    cout <<endl;"nhap day cac phan so: ";
     for(int i =0;i<n;i++)
     p[i].nhap();
     cout <<endl<<"day phan so vua nhap: ";
@@ -88,7 +122,7 @@ int main ()
     for(int i=0;i<n;i++)
     for(int j=0;j<n;j++)
     {
-        if(so.sosanh(p[j],p[i]))
+        if(p[j]<p[i])
         {
             PHANSO temp = p[i];
                     p[i] = p[j];
@@ -101,5 +135,11 @@ int main ()
     (p+i)->in();
     cout <<" ";
     }
+    cout <<endl<<"doi dau day so: " ;
+    for(int i=0;i<n;i++)
+    {
+        PHANSO doidau = -p[i];
+        doidau.in();
+        cout <<" ";
+    }
 }
-
